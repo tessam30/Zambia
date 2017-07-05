aa60686c44ac84280196577de11a2cc33bb67f17
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-05 15:33:25 -0400

    update Shiwang'Andu merging issue

--- ZMB_import_geo.R
+++ ZMB_import_geo.R
@@ -1,6 +1,10 @@
 
+# Import Zambia geographic data -------------------------------------------
+# Laura Hughes, lhughes@usaid.gov, USAID | GeoCenter, 5 July 2017
+
+
 # Installation instructions -----------------------------------------------
-# To install on Mac, have to first get GDAL, PROJ, GEOS (from QGIS installation, via KChang...)
+# To install on Mac, have to first get GDAL, PROJ, GEOS (from QGIS installation, via http://www.kyngchaos.com/software/qgis)
 # Will install in library/Frameworks
 # Had to use Homebrew to get PROJ linked in right location: brew install proj
 # https://github.com/edzer/sfr
@@ -9,18 +13,33 @@
 # 
 # then at terminal: R CMD INSTALL sf_0.5-1.tar.gz --configure-args='-with-gdal-config=/Library/Frameworks/GDAL.framework/Versions/2.1/unix/bin/gdal-config -with-geos-config=/Library/Frameworks/GEOS.framework/Versions/3/unix/bin/geos-config' 
 
+
+# setup -------------------------------------------------------------------
+
+
 library(sf)
 library(ggplot2)
 library(dplyr)
+library(readxl)
+library(data.table)
 
 base_dir = '~/Documents/GitHub/Zambia/'
 
 # import master name list. ------------------------------------------------
+# Creates a crosswalk between the shapefile names and those used on election website (with vote count)
+# Also connects the 150 constituencies from pre-2016 to the 156 afterwards.
+# Note that while the names may be the same, the boundaries have shifted and in some cases are quite different.
 geo_base = read_excel(paste0(base_dir, 'ZMB_admin_crosswalk.xlsx'), sheet = 2)
+
+
+# import / clean geographic constituency data -----------------------------
 
 # import data
 zmb15 = st_read(paste0(base_dir, 'geodata/constituencies/'), layer = 'GRED_Zambia_2006_beta2')
 zmb16 = st_read(paste0(base_dir, 'geodata/constituencies/'), layer = 'Zambia_Const_156')
+
+# fix "Shiwang\x92Andu" -- gives merging issues
+zmb15 = zmb15 %>% mutate(ConstName = ifelse(as.character(CST_N) %like% 'Shiwang', "Shiwang'Andu", as.character(CST_N)))
 
 # re-project
 proj_str = '+proj=laea +lon_0=27.509765625'
@@ -32,7 +51,11 @@ zmb15 = st_transform(zmb15, proj_str)
 # isect = st_intersection(zmb15, zmb16)
 
 
+# quick plot --------------------------------------------------------------
+
 p = ggplot(zmb16) + geom_sf()
 
-zmb15 = left_join(zmb15, geo_base, by = c('CST_N' = "shp2015"))
+
+# merge crosswalk data ----------------------------------------------------
+zmb15 = left_join(zmb15, geo_base, by = c('ConstName' = "shp2015"))
 zmb16 = left_join(zmb16, geo_base, by = c('ConstName1' = "shp2016"))
