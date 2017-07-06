15cb13cf78b1e7eb14b48e06c796fbed5eb79659
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-06 09:18:24 -0400

    Merge branch 'master' of https://github.com/tessam30/Zambia

---
+++ ZMB_geo_crosswalk.R
@@ -0,0 +1,100 @@
+
+# Import Zambia election constituency shpfiles ----------------------------
+# Imports shape files for Zambia's election data
+# Laura Hughes, USAID | GeoCenter, 5 July 2017, lhughs@usaid.gov
+
+
+# Sources -----------------------------------------------------------------
+# Zambia election constituencies were split from 150 constituencies to 156 for 2016 election. (https://zambiareports.com/2015/05/13/cabinet-creates-six-new-constituencies/)
+# 1) Zambia 2016 Constituencies source: https://github.com/lightonphiri/data-zambia-shapefiles/tree/master/cso-shapefiles
+# Based on: http://lightonphiri.org/blog/mapping-the-zambia-2016-presidential-election-results
+# 2) Zambia pre-2016 Constituencies from U. Michigan Constituency-Level Elections Archive
+# http://www.electiondataarchive.org/datacenter-gred.html
+# 3) Zambia district, province data from John Booz, from Ministry of Land, circa 2015.
+
+
+# setup -------------------------------------------------------------------
+
+library(geocenter)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(readxl)
+
+base_dir = '~/Documents/GitHub/Zambia/'
+
+# PROJ-4 string to reproject data before converting it to lat/lons
+# Equatorial Lambert azimuthal equal-area PROJ.4
+# Central meridian: 027º 30' E
+proj_str = '+proj=laea +lon_0=27.509765625'
+
+# import shp --------------------------------------------------------------
+
+zmb156 = shp2df(baseDir = paste0(base_dir, 'geodata/constituencies/'), 
+                layerName = 'Zambia_Const_156', getCentroids = FALSE,
+                reproject = TRUE, projection = proj_str)
+
+zmb150 = shp2df(baseDir = paste0(base_dir, 'geodata/constituencies/'), 
+                layerName = 'GRED_Zambia_2006_beta2', getCentroids = FALSE,
+                reproject = TRUE, projection = proj_str)
+
+
+# Pull out unique names ---------------------------------------------------
+
+zmb156 = zmb156 %>% 
+  mutate(shp2016 = str_to_title(ConstName1))
+
+zmb150 = zmb150 %>% 
+  mutate(shp2015 = str_to_title(CST_N))
+
+
+cst156 = zmb156 %>% select(shp2016) %>% mutate(constituency = shp2016) %>% distinct()
+cst150 = zmb150 %>% select(shp2015) %>% mutate(constituency = shp2015) %>% distinct() 
+
+# Merging, crosswalks -----------------------------------------------------
+
+# from ZMB_assembly2016_clean.R
+cst156_website = read.csv('~/Documents/GitHub/Zambia/processeddata/ZMB_2016_adminnames.csv') %>% select(-X)
+
+# from ZMB_pres2015_clean.R
+cst150_website = pres15 %>% select(constName) %>% distinct()
+
+cst156_crosswalk = full_join(cst156, cst156_website %>% mutate(website2016 = constituency), 
+                             by = c("constituency" = "constituency"))
+
+write.csv(cst156_crosswalk, '~/Documents/GitHub/Zambia/processeddata/cst156_crosswalk.csv')
+
+
+cst150_crosswalk = full_join(cst150, cst150_website %>% mutate(website2015 = constName), by = c("constituency" = "constName"))
+write.csv(cst150_crosswalk, '~/Documents/GitHub/Zambia/processeddata/cst150_crosswalk.csv')
+
+crosswalk = full_join(cst150_crosswalk, cst156_crosswalk, by = c("constituency"))
+
+crosswalk = crosswalk %>% mutate(merge_flag = ifelse(is.na(website2015) | is.na(website2016) | 
+                                                       is.na(shp2015) | is.na(shp2016), 1, 0))
+
+write.csv(crosswalk, '~/Documents/GitHub/Zambia/processeddata/cst_crosswalk.csv')
+
+# After identifying where the discrepancies are, merged by hand into ZMB_admin_crosswalk.xlsx
+# Used Zambia Electoral Commission's Public Notice on 7 May 2016 for changes to constituencies
+# Merged by hand w/ similar names, double checking with overlaid shapefiles.
+# !!! NOTE that shapefiles b/w pre-2016 and post-2016 do not exactly overlay, and some parts of the districts were cut into pieces.
+
+
+# import master name list. ------------------------------------------------
+geo_base = read_excel(paste0(base_dir, 'ZMB_admin_crosswalk.xlsx'), sheet = 2)
+
+
+# merge to shpfiles
+zmb150 = left_join(zmb150, geo_base, by = c("shp2015"))
+
+zmb156 = left_join(zmb156, geo_base, by = c("shp2016"))
+
+
+# plot --------------------------------------------------------------------
+# p = ggplot(zmb156, aes(x = long, y = lat, fill = district, group = group, order = order)) +
+#   geom_polygon() +
+#   geom_path(size = 0.1, color = 'white') +
+#   coord_equal() +
+#   theme_void() +
+#   theme(legend.position = 'none')
--- ZMB_pres2011_clean.R
+++ ZMB_pres2011_clean.R
@@ -1,13 +1,74 @@
+
+# Import and clean the 2011 Zambia Presidental Election results -----------
+# At the constituency level
+# Laura Hughes, lhughes@usaid.gov, USAID | GeoCenter
+
 library(readxl)
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(readr)
 
 
 # import data -------------------------------------------------------------
 # data pulled from http://www.elections.org.zm/media/28092011_public_notice_-_2011_presidential_election_results.pdf
 # on 29 June 2017
-# Exported from Adobe Acrobat into Excel to begin the cleaning process
+# Extracted from pdf using Tabula (http://tabula.technology/) to begin the cleaning process
 
-pres2011 = read_excel('rawdata/2011_presidential_election_results.xlsx', skip = 15)
+pr11_raw = read_csv('rawdata/tabula-2011_presidential_election_results.csv')
 
+# glance at structure
+glimpse(pr11_raw)
+
+# check if columns are necessary
+t(pr11_raw %>% summarise_all(funs(sum(!is.na(.)))))
+
+# So, annoyingly, the data is structured as such:
+# candidate_region contains an initial row specifying the constituency name
+# then are the constituency vote totals by party
+# followed by the constituency summary totals
+
+pr11 = pr11_raw %>% 
+  mutate(
+    # constituencies are all labeled by numbers (yay order);
+    # province totals specified by "Province" (though tried to remove all of them so they don't muck things up)
+    constit = ifelse(str_detect(candidate_region, '[0-9]') | str_detect(candidate_region, 'Province'), candidate_region, NA),
+    # constituency totals have NA in the candidate regions
+    total = ifelse(is.na(candidate_region), 1, 0),
+    # convert votes to numeric
+    votes = as.numeric(str_replace_all(Votes, ',', '')),
+    year = 2011
+  ) %>% 
+  # fill down constituency names
+  fill(constit) %>% 
+  # split constit into id and name
+  separate(constit, into = c('constit_id', 'constit1', 'constit2'), sep = ' ') %>%
+  # make constit purty
+  mutate(constituency = ifelse(is.na(constit2), str_to_title(constit1),
+                               str_to_title(paste(constit1, constit2))))
+
+
+# check import looks right ------------------------------------------------
+# should all have 12 
+View(pr11 %>% count(constit_id))
+
+# totals at the constituency level; also includes number of rejected votes
+pr11_total = pr11 %>% 
+  filter(total == 1,
+         # remove extra text taken along for the ride
+         !is.na(votes)
+         ) %>% 
+    mutate(rejected = as.numeric(str_replace_all(rejected_ballotpapers, ',', '')),
+           cast = as.numeric(str_replace_all(total_votes, ',', '')),
+           registered = as.numeric(str_replace_all(total_registered, ',', '')),
+           pct_rejected_lab = pct_rejected,
+           pct_rejected = rejected/cast,
+           turnout = cast / registered,
+           valid_turnout = votes / registered
+    )
+
+# After check by eye, pct_rejected calc looks on target with what Zambia reports; dropping their formatted number
+# pct_poll is equivalent to turnout.
+  select(constit_id, constituency, year, registered, cast, votes,
+         rejected, pct_rejected, turnout, valid_turnout)
+  
