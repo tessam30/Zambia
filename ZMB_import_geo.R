9ce05dd7316f9e12a60813f4bdf2aad8e2f8ad43
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-07 16:12:46 -0400

    cleanup

--- ZMB_import_geo.R
+++ ZMB_import_geo.R
@@ -7,11 +7,13 @@
 # To install on Mac, have to first get GDAL, PROJ, GEOS (from QGIS installation, via http://www.kyngchaos.com/software/qgis)
 # Will install in library/Frameworks
 # Had to use Homebrew to get PROJ linked in right location: brew install proj
+# Also requires XCode to be installed (to compile C)
 # https://github.com/edzer/sfr
 # https://github.com/edzer/sfr/issues/335
 # https://stat.ethz.ch/pipermail/r-sig-mac/2017-June/012429.html
 # 
-# then at terminal: R CMD INSTALL sf_0.5-1.tar.gz --configure-args='-with-gdal-config=/Library/Frameworks/GDAL.framework/Versions/2.1/unix/bin/gdal-config -with-geos-config=/Library/Frameworks/GEOS.framework/Versions/3/unix/bin/geos-config' 
+# then at terminal: (default location seems to be unlinked when try to install from within RStudio)
+# R CMD INSTALL sf_0.5-1.tar.gz --configure-args='-with-gdal-config=/Library/Frameworks/GDAL.framework/Versions/2.1/unix/bin/gdal-config -with-geos-config=/Library/Frameworks/GEOS.framework/Versions/3/unix/bin/geos-config' 
 
 
 # setup -------------------------------------------------------------------
@@ -21,6 +23,7 @@ library(sf)
 library(ggplot2)
 library(dplyr)
 library(readxl)
+library(stringr)
 library(data.table)
 
 base_dir = '~/Documents/GitHub/Zambia/'
@@ -39,7 +42,8 @@ zmb15 = st_read(paste0(base_dir, 'geodata/constituencies/'), layer = 'GRED_Zambi
 zmb16 = st_read(paste0(base_dir, 'geodata/constituencies/'), layer = 'Zambia_Const_156')
 
 # fix "Shiwang\x92Andu" -- gives merging issues
-zmb15 = zmb15 %>% mutate(ConstName = ifelse(as.character(CST_N) %like% 'Shiwang', "Shiwang'Andu", as.character(CST_N)))
+zmb15 = zmb15 %>% mutate(ConstName = ifelse(as.character(CST_N) %like% 
+                                              'Shiwang', "Shiwang'Andu", as.character(CST_N)))
 
 # title case convert constituency name, for merging
 zmb16 = zmb16 %>% mutate(shp2016 = str_to_title(ConstName1))
@@ -54,11 +58,14 @@ zmb15 = st_transform(zmb15, proj_str)
 # isect = st_intersection(zmb15, zmb16)
 
 
-# quick plot --------------------------------------------------------------
-
-p = ggplot(zmb16) + geom_sf()
 
 
 # merge crosswalk data ----------------------------------------------------
 zmb15 = left_join(zmb15, geo_base, by = c('ConstName' = "shp2015"))
 zmb16 = left_join(zmb16, geo_base, by = c('shp2016' = "shp2016"))
+
+
+# quick plot --------------------------------------------------------------
+plot(zmb16)
+
+p = ggplot(zmb16) + geom_sf(aes(fill = constituency), show.legend = FALSE) 
