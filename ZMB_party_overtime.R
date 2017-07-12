83ce1b14c094a8dc389cea679b17a5bae5a05b90
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-12 15:33:05 -0400

    calculate party pct over time

---
+++ ZMB_party_overtime.R
@@ -0,0 +1,28 @@
+library(forcats)
+
+maj_parties = c('PF', 'UPND', 'MMD')
+
+party_tot = pr_votes %>% 
+  mutate(party_grp = ifelse(party %in% maj_parties, party, 'other')) %>% 
+  group_by(party_grp, year) %>% 
+  summarise(total = sum(vote_count)) %>% 
+  ungroup() %>% group_by(year) %>% 
+  mutate(pct = total/sum(total)) %>% 
+  arrange(desc(total))
+
+party_order = unique(party_tot$party_grp)
+
+party_tot$party_grp = factor(party_tot$party_grp, levels = party_order)
+
+
+ggplot(party_tot, aes(x = year, y = pct)) +
+  geom_area(stat = 'identity', alpha = 0.3) +
+  scale_y_continuous(labels = scales::percent) + 
+  facet_wrap(~party) +
+  theme_bw()
+
+ggplot(party_tot, aes(x = year, y = pct, fill = party_grp)) +
+  geom_bar(stat = 'identity', alpha = 1) +
+  scale_y_continuous(labels = scales::percent) + 
+  theme_bw()
+
