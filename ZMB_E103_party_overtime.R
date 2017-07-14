4f4e7700b61eaf93941458d75138cb47c05e6108
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-14 11:39:10 -0400

    raw svg

--- ZMB_E103_party_overtime.R
+++ ZMB_E103_party_overtime.R
@@ -1,5 +1,14 @@
 
-source('ZMB_E201_pct_byparty.R')
+# Area graph over time ----------------------------------------------------
+# Change in share of electoral vote over time, by party
+# Laura Hughes, lhughes@usaid.gov, USAID | GeoCenter, 14 July 2017
+# In the end, made with RAW graphics and imported into AI.
+
+
+
+# import data -------------------------------------------------------------
+# source('ZMB_pres_mergeAll.R')
+# source('ZMB_E201_pct_byparty.R')
 
 party_order = unique(party_tot$party_grp)
 
@@ -11,7 +20,7 @@ ggplot(party_tot, aes(x = year, y = pct, fill = color)) +
   scale_y_continuous(labels = scales::percent) + 
   scale_fill_identity() +
   # facet_wrap(~party_grp) +
-  theme_bw()
+  theme_ygrid()
 
 ggplot(party_tot, aes(x = year, y = pct, fill = party_grp)) +
   geom_bar(stat = 'identity', alpha = 1) +
