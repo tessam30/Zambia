f6c3088082108853e396590081547f3f1fd4a0d4
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-19 17:23:22 -0400

    prelim exploration of changes in party affiliations

---
+++ ZMB_E102_partyAffiliation.R
@@ -0,0 +1,33 @@
+
+# look at changes in party affiliation between candidates -----------------
+
+affil = as_votes %>% 
+  select(candidate, year, party, constituency) %>% 
+  spread(year, party) %>% 
+  filter(!is.na(`2016`), !is.na(`2011`)) %>% 
+  mutate(partyChg = `2016` == `2011`)
+
+affil %>% filter(partyChg == 0) %>% count(`2011`, `2016`)
+
+# Create a unique ID to track the 
+as_affil = pr_votes %>% 
+  mutate(candidID = dense_rank(paste(candidate, constituency))) %>% 
+  group_by(candidID) %>% 
+  mutate(num_elections = n()) %>% 
+  ungroup() %>% 
+  filter(num_elections > 1) %>% 
+  group_by(party) %>% 
+  mutate(party_ct = n())
+
+pr_votes = pr_votes
+
+affil = pr_votes %>% 
+  select(candidate, year, party, constituency) %>% 
+  spread(year, party) %>% 
+  mutate(partyChg = `2016` == `2011`)
+
+ggplot(as_affil, aes(x = year, y = fct_reorder(party, party_ct),
+                                                   group = candidID)) +
+  geom_line(size = 3, alpha = 0.2) +
+  ggtitle('Change in party between Parliamentary elections') +
+  theme_minimal()
