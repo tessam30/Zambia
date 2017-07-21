39e505057189115684a474a402acda4bd322d63e
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-21 17:24:17 -0400

    change pct to be cast percent

--- ZMB_E101_calcpct_byparty.R
+++ ZMB_E101_calcpct_byparty.R
@@ -16,21 +16,32 @@ maj_parties = c('PF', 'UPND', 'UPND, FDD, UNIP (UDA)', 'MMD')
 
 total_byparty = function(df) {
   
+  # total number of votes cast. duplicated across all political parties, 
+  # so selecting unique total cast per constituency and adding
+  cast_total = df %>% 
+    select(year, constituency, cast) %>% 
+    filter(!is.na(cast)) %>% 
+    distinct() %>% 
+    group_by(year) %>% 
+    summarise(cast_total = sum(cast))
+  
   df %>% 
     # lump parties getting few votes into "other" category
     mutate(party_grp = ifelse(party %in% maj_parties, party, 'other')) %>% 
     
     # group by party
-    group_by(party_grp, year, color) %>% 
+    group_by(party_grp, party_name, year, color) %>% 
     
     # calculate total # votes by party
     summarise(total = sum(vote_count)) %>% 
     
     # calculate percent of the total, national party
-    ungroup() %>% group_by(year) %>% 
-    mutate(pct = total/sum(total),
-           pct_lab = percent(pct, 0),
-           party = party_grp) %>% 
+    ungroup() %>% 
+    left_join(cast_total, by = 'year') %>% 
+    group_by(year) %>% 
+    mutate(pct_valid = total/sum(total),
+           pct = total / cast_total,
+           pct_lab = percent(pct, 0)) %>% 
     arrange(desc(total))
 }
 
@@ -42,6 +53,11 @@ write_csv(pres_tot, paste0(data_dir, 'ZMB_presvotes_byparty.csv'))
 
 
 parl_tot = as_votes %>% total_byparty()
+
+as_winners = as_votes %>% 
+  filter(won == 1) %>% 
+  count(party_name, color, year) %>% 
+  arrange(year, desc(n))
 
 # export
 write_csv(parl_tot, paste0(data_dir, 'ZMB_assemblyvotes_byparty.csv'))
