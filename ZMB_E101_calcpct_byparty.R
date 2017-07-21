fb860f0f5a8ae3ca0a06b665e47dd51fe365a028
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-21 14:34:22 -0400

    update calcs w/ assembly data

--- ZMB_E101_calcpct_byparty.R
+++ ZMB_E101_calcpct_byparty.R
@@ -14,25 +14,37 @@ maj_parties = c('PF', 'UPND', 'UPND, FDD, UNIP (UDA)', 'MMD')
 
 # Presidential elections, 2006-2016. --------------------------------------
 
-party_tot = pr_votes %>% 
-  # lump parties getting few votes into "other" category
-  mutate(party_grp = ifelse(party %in% maj_parties, party, 'other')) %>% 
+total_byparty = function(df) {
   
-  # group by party
-  group_by(party_grp, year, color) %>% 
-  
-  # calculate total # votes by party
-  summarise(total = sum(vote_count)) %>% 
-  
-  # calculate percent of the total, national party
-  ungroup() %>% group_by(year) %>% 
-  mutate(pct = total/sum(total),
-         pct_lab = percent(pct, 0),
-         party = party_grp) %>% 
-  arrange(desc(total))
+  df %>% 
+    # lump parties getting few votes into "other" category
+    mutate(party_grp = ifelse(party %in% maj_parties, party, 'other')) %>% 
+    
+    # group by party
+    group_by(party_grp, year) %>% 
+    
+    # calculate total # votes by party
+    summarise(total = sum(vote_count)) %>% 
+    
+    # calculate percent of the total, national party
+    ungroup() %>% group_by(year) %>% 
+    mutate(pct = total/sum(total),
+           pct_lab = percent(pct, 0),
+           party = party_grp) %>% 
+    arrange(desc(total))
+}
+
+pres_tot = pr_votes %>% total_byparty()
+# export
+write_csv(pres_tot, paste0(data_dir, 'ZMB_presvotes_byparty.csv'))
+
+# Assembly elections ------------------------------------------------------
+
+
+parl_tot = as_votes %>% total_byparty()
 
 # export
-write_csv(party_tot, paste0(data_dir, 'ZMB_presvotes_byparty.csv'))
+write_csv(parl_tot, paste0(data_dir, 'ZMB_assemblyvotes_byparty.csv'))
 
 
 
